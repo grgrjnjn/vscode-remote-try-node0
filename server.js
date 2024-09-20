50dda976b035792e76ac6ffbd9fff8660ccb4f5d
grgrjnjn <grgrjnjn@gmail.com>
2024-09-20 08:52:55 +0000

    server.js形式変換

--- server.js
+++ server.js
@@ -1,6 +1,13 @@
-const express = require('express');
-const fs = require('fs').promises;
-const path = require('path');
+// CommonJS形式からESmodules形式に変更してください。
+
+
+import express from 'express';
+import { promises as fs } from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const app = express();
 const PORT = 3000;
